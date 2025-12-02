213ea2be1dcfdd8791a98259c701798be09e98c6
minene-hm <ahmiaminene69@gmail.com>
2025-12-02 17:09:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import Events from './pages/Events';
